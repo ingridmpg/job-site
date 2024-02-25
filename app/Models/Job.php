2bcc7be8587f017b9971e083fa3dd13535dcd32e
ingridmpg <ingrid.grelo@gmail.com>
2024-02-25 13:15:34 +0000

    Update model and migration

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,30 +2,10 @@
 
 namespace App\Models;
 
-class Job
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
 {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'job one',
-                'description' => 'Lorem ipsum dolor sit amet. Non tempore accusamus sed deleniti iusto eum fugiat dolor ut aspernatur eius. Et dolores eveniet sit quas fuga nam odio delectus aut beatae tempore quo delectus animi qui ullam impedit vel voluptatibus dignissimos.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'job two',
-                'description' => 'Lorem ipsum dolor sit amet. Non tempore accusamus sed deleniti iusto eum fugiat dolor ut aspernatur eius. Et dolores eveniet sit quas fuga nam odio delectus aut beatae tempore quo delectus animi qui ullam impedit vel voluptatibus dignissimos.'
-            ]
-        ];
-    }
-
-    public static function find($id) {
-        $jobs = self::all();
-
-        foreach ($jobs as $job) {
-            if ($job['id'] == $id) {
-                return $job;
-            }
-        }
-    }
+    use HasFactory;
 }
