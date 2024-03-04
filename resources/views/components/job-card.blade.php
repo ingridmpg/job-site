eb8f9cadda53398d0409eda00c24d874375fbcba
ingridmpg <ingrid.grelo@gmail.com>
2024-03-04 19:53:15 +0000

    Create Components

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,6 +1,6 @@
 @props(['job'])
 
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<x-card>
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
@@ -12,31 +12,12 @@
                 <a href="/jobs/{{$job->id}}">{{$job->title}}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{$job->company}}</div>
-            <ul class="flex">
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Laravel</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">API</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Backend</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+
+            <x-job-tags :jobTags="$job->tags" />
+
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i> {{$job->location}}
             </div>
         </div>
     </div>
-</div>
+</x-card>
